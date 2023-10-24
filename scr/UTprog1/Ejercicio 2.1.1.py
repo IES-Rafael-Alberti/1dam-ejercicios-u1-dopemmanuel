e06202f4333b9e0c599930117d3f7fc014338d8b
dopemmanuel <148891292+dopemmanuel@users.noreply.github.com>
2023-10-24 17:30:53 -0400

    Editing

--- scr/UTprog1/Ejercicio 2.1.1.py
+++ scr/UTprog1/Ejercicio 2.1.1.py
@@ -12,4 +12,21 @@ elif edad > 18:
     print("Eres mayor de edad")
 """
 
-def 
+def verificar_edad():
+    
+    while True:
+        try:
+            edad = int(input("Ingrese su edad: "))
+
+                    #Esta es opcional XD
+            if edad > 100:
+                    print("En serio??!!", edad, "años??, tu deverias de estar muerto eso es imposible...")
+        
+            elif  edad < 18:
+                    print("Eres menor de edad")
+      
+            elif edad > 18:
+                    print("Eres mayor de edad")
+            break
+        except ValueError:
+              print("Porfa ingresa tu verdadera edad...")
