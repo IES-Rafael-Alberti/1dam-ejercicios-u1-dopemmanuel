bc9d192b71de2c791922c3920bef48b319210c3d
dopemmanuel <dopemmanuel@iesrafaelalberti.es>
2023-10-26 14:51:00 -0400

    j

--- scr/ej32.py
+++ scr/ej32.py
@@ -1,15 +1,18 @@
 num1 = int(input("Introduce un número: "))
 num2 = int(input("Introduce otro número: "))
 
-if (num1 >= num2):
+if num1 >= num2:
     numIni = num1
     numFin = num2
 else:
     numIni = num2
     numFin = num1
 
-while (numIni <= numFin):
-    print(numIni, end="")
-    if (numIni != numFin):
-        print("-", end="")
-numIni = numIni + 1
+serie: str = ""
+while numIni <= numFin:
+    serie = serie + numIni
+    if numIni != numFin:
+        serie = serie + "-"
+    numIni = numIni + 1
+
+print(serie)
